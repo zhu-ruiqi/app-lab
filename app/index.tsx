02a76307fc76fd8c27c050271d0ec9c5808889fa
zhu-ruiqi <206345965+zhu-ruiqi@users.noreply.github.com>
2025-04-16 02:45:29 +0800

    FINALLYgit add .! 32:23

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Ricky</Text>
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-5xl font-bold text-blue-500">Hello world!</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
