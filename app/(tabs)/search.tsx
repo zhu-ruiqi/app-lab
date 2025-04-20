cc5ea85d4434e01d3edb72733b505630b6e9c200
zhu-ruiqi <206345965+zhu-ruiqi@users.noreply.github.com>
2025-04-21 00:47:35 +0800

    Refactor Search component to include movie fetching and display with FlatList

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,53 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, FlatList } from "react-native";
 import React from "react";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import { useRouter } from "expo-router";
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
 
 const Search = () => {
+  const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    fetchMovies({
+      query: "",
+    })
+  );
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "space-between",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+          </>
+        }
+      />
     </View>
   );
 };
